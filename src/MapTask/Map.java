f271611a1217d1dd125f8ac85792375c08e1292f
samibyrone <samsonibironke2@gmail.com>
2024-10-14 00:22:11 +0100

    map (Data-Structure) commit

--- src/MapTask/Map.java
+++ src/MapTask/Map.java
@@ -1,13 +1,56 @@
 package MapTask;
 
-public class Map {
+import java.util.HashMap;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
+public class Map <Plus, Minus> {
 
+    private Map<Plus, Minus> map;
+
+    public Map() {
+        map = new Map<>();
+    }
     public boolean isEmpty() {
-        return map.isEmpty;
+        return map.isEmpty();
     }
 
     public int size() {
-        return size;
+        return map.size();
     }
+
+    public void put(Plus plus, Minus minus) {
+        map.put(plus, minus);
+    }
+
+    public void replace(Plus plus, Minus minus) {
+        if(map.containsKey(plus)) {
+            map.put(plus, minus);
+        }
+    }
+
+    public void clear(){
+        map.clear();
+    }
+
+    public void compute(Plus plus, Function<? super Minus, ? extends Minus> remappingFunction){
+        map.compute(plus, remappingFunction);
+    }
+
+    public void computeIfPresent(Plus plus, Function<? super Minus, ? extends Minus> remappingFunction){
+        map.computeIfPresent(plus, remappingFunction);
+    }
+
+    public void computeIfAbsent(Plus plus, Supplier<? extends Minus> mappingFunction){
+        map.computeIfAbsent(plus, Plus -> mappingFunction.get());
+    }
+
+    public boolean containsKey(Plus key) {
+        return map.containsKey(key);
+    }
+
+    public boolean containsValue(Plus value) {
+        return map.containsValue(value);
+    }
+
 }
